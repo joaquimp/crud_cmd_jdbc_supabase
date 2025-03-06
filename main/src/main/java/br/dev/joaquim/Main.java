7e932111660083ba0a1af864a78aa3ff5c150391
Joaquim Pessôa Filho <joaquimp@users.noreply.github.com>
2025-03-06 13:53:30 +0000

    versao feita em aula

--- main/src/main/java/br/dev/joaquim/Main.java
+++ main/src/main/java/br/dev/joaquim/Main.java
@@ -4,20 +4,26 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            String pwd = "buVrib-qitko0-nunfiq"; // mudo depois da aula!!!
+            String pwd = "iwvJtIrdorlgtsXQ"; // mudo depois da aula!!!
             String url = "jdbc:postgresql://aws-0-us-west-1.pooler.supabase.com:5432/postgres?user=postgres.mfmmwdiptsysdiohifpi&password=" + pwd;
             Connection conn = DriverManager.getConnection(url);
 
-            String query = "select * from contas";
+            Scanner entrada = new Scanner(System.in);
+            System.out.print("Informe o numero da conta: ");
+            int nroConta = Integer.parseInt(entrada.nextLine());
+
+            String query = "select * from contas where nro_conta = ?";
             PreparedStatement pstmt = conn.prepareStatement(query);
+            pstmt.setInt(1, nroConta);
             ResultSet rs = pstmt.executeQuery();
 
             while(rs.next()) {
-                System.out.print(rs.getDouble("nro_conta"));
+                System.out.print(rs.getInt("nro_conta"));
                 System.out.print(" - ");
                 System.out.println(rs.getDouble("saldo"));
             }
